f3151697d02919be78e54bbb40aef32d87fae5be
hottonlira <hottonlucasfarialira@gmail.com>
2026-02-19 15:51:23 +0000

    criei cartas super trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,11 +5,72 @@
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
 int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+    // carta 1
+    char  estado1;
+    char  codigo1[4];
+    char  cidade1[50];
+    int   populacao1[50];
+    float area1;
+    float pib1;
+    int   pontos1;
 
-  // Área para entrada de dados
+    // carta 2
 
-  // Área para exibição dos dados da cidade
+    char  estado2;
+    char  codigo2[4];
+    char  cidade2[50];
+    int   populacao2;
+    float area2;
+    float pib2;
+    int   pontos2;
 
+    // entrada carta 1
+
+    printf("=== cadastro da carta 1 ===\n");
+    printf("estado (A a h): ");
+    scanf("%c", &estado1);
+
+    printf("codigo da carta (ex: A01)");
+    scanf("%s", codigo1);
+
+    printf("nome da cidade: ");
+    scanf("%[^\n]", cidade1);
+
+    printf("populacao: ");
+    scanf("%d", &populacao1);
+
+    printf("area (km2): ");
+    scanf("%f", &area1);
+
+    printf("pib (em bilhoes): ");
+    scanf("%f", &pib1);
+
+    printf("numero de pontos turisticos: ");
+    scanf("%d", &pontos1);
+
+    // entrada da carta 2
+    printf("\n=== cadastro da cartsa 2 ===\n");
+    printf("estado (A a H): ");
+    scanf("%c", &estado2);
+
+    printf("codigo da carta (ex: B02): ");
+    scanf("%s", codigo2);
+
+    printf("nome da cidade: ");
+    scanf("%[^\n]", cidade2);
+
+    printf("populacao: ");
+    scanf("%d", &populacao2);
+
+    printf("area (km2): ");
+    scanf("%f", &area2);
+
+    printf("pib (em bilhoes): ");
+    scanf("%f", &pib2);
+
+    printf("numero de pontos turisticos:");
+    scanf("%d", &pontos2);
+
+    
 return 0;
 } 
